eaba42251f782dfaa23e22382ec41b29c6aa303a
stan lap <stanlapen5@gmail.com>
2024-02-25 13:18:35 +0300

    Initial commit

---
+++ android/src/com/mk/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mk.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mk.game.MkGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MkGame(), config);
+	}
+}
